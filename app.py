2962b7c96744718aa0916495af0c656f42b84b1c
Theodore Rand <randtheodore@gmail.com>
2020-12-17 01:03:21 -0600

    added CAFC tables

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-import flask
+import flask, json
 app= flask.Flask(__name__)
+
+cafc_opinions = json.load(open('./data/cafc_opinions.json'))
+cafc_hearings = json.load(open('./data/cafc_hearings.json'))
 
 @app.route('/')
 def index():
-  return flask.render_template('index.html')
+  return flask.render_template('index.html', 
+    cafc_opinions=cafc_opinions,
+    cafc_hearings=cafc_hearings)
 
 if __name__ == "__main__":
     app.run()
